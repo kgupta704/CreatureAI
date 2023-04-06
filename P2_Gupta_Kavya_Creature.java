995a544864b56c1acb74d3c705c2dbbffddf37e7
KGupta10 <imkavyagupta@gmail.com>
2023-04-05 23:09:50 -0700

    added some not working code to go against walls
    does not work at all, no clue whats happening

--- P2_Gupta_Kavya_Creature.java
+++ P2_Gupta_Kavya_Creature.java
@@ -12,13 +12,21 @@ public class P2_Gupta_Kavya_Creature extends Actor {
     int speed = 2;
     Treat target = null;
     boolean wander = false;
-    int wanderDirec;
+    //int wanderDirec;
+    Wall wall;
+    int wanderRot=90;
+    int starting = 1;
 
     public void act() {
-        if(isTouching(Wall.class)) {
-            System.out.println("bad start");
-            //setLocation(startX, startY);
+        if(starting==1) {
+            wall = getWorld().getObjects(Wall.class).get(0);
+            //starting=0;
         }
+
+        // if(isTouching(Wall.class)) {
+            // System.out.println("bad start");
+            // //setLocation(startX, startY);
+        // }
         int startX = getX();
         int startY = getY();
         if(closestReachTreat() != null) {
@@ -38,27 +46,87 @@ public class P2_Gupta_Kavya_Creature extends Actor {
                 target = null;
             }
         } else {
-            int randNum = Greenfoot.getRandomNumber(100);
-            if(!wander || randNum == 0) {
-                wanderDirec = returnDirection();
+            if(!touchingWall()) {
+                turnTowards(wall.getX(), wall.getY());
+                move(1);
+                if(touchingWall()) {
+                    wanderRot = changeWanderRot(wanderRot);
+                    wall = wallAtDirection(wanderRot);
+                }
+            } else {
+                if(!directionIsClear(wanderRot)) {
+                    wanderRot = changeWanderRot(wanderRot);
+                }
+                setRotation(wanderRot);
+                if(!directionIsClear(wanderRot)) {
+                    System.out.println("bad");
+                }
+                move(1);
             }
-            
-            if(!directionIsClear(wanderDirec)) { 
-                wanderDirec = returnDirection();
-            }
-            setRotation(wanderDirec);
-            move(1);
-            setRotation(0);
-            wander = true;
-            //if(getOneIntersectingObject(Wall.class) != null) {
-                //dontTouchWall(wanderDirec);
-            //}
+
         }
         setRotation(0);
         if(isTouching(Wall.class)) {
             System.out.println("bad");
             //setLocation(startX, startY);
         }
+    }
+
+    public int changeWanderRot(int rot) {
+        if(rot == 90) {
+            if(!directionIsClear(0)) {
+                rot = 180;
+                wall = wallAtDirection(90);
+            } else if(!directionIsClear(180)) {
+                rot = 0;
+                wall = wallAtDirection(90);
+            } else if(!directionIsClear(270)){
+                rot = 270;
+                wall = wallAtDirection(0);
+            }
+            //setLocation(getX(), getY()-1);
+        } else if(rot == 0){
+            if(!directionIsClear(90)){
+                rot = 270;
+                wall = wallAtDirection(0);
+            } else if(!directionIsClear(270)) {
+                rot = 90;
+                wall = wallAtDirection(0);
+            } else if(!directionIsClear(180)){
+                rot = 180;
+                wall = wallAtDirection(90);
+            }
+            //setLocation(getX()+1, getY());
+        } else if(rot == 180){
+            if(!directionIsClear(90)){
+                rot = 270;
+                wall = wallAtDirection(180);
+            } else if(!directionIsClear(270)) {
+                rot = 90;
+                wall = wallAtDirection(180);
+            } else if(!directionIsClear(0)){
+                rot = 0;
+                wall = wallAtDirection(90);
+            }
+            //setLocation(getX()-1, getY());
+        } else if(rot == 270) {
+            if(!directionIsClear(0)) {
+                rot = 180;
+                wall = wallAtDirection(270);
+            } else if(!directionIsClear(180)) {
+                rot = 0;
+                wall = wallAtDirection(270);
+            } else if(!directionIsClear(90)){
+                rot = 270;
+                wall = wallAtDirection(0);
+            }
+            //setLocation(getX(), getY()+1);
+        }
+        return rot;
+    }
+
+    public boolean touchingWall() {
+        return getOneIntersectingObject(Wall.class) != null;
     }
 
     public void dontTouchWall(int startRot) {
@@ -71,7 +139,7 @@ public class P2_Gupta_Kavya_Creature extends Actor {
                 setRotation(0);
             }
         }
-        setRotation(startRot);
+        setRotation(0);
 
     }
 
@@ -144,19 +212,19 @@ public class P2_Gupta_Kavya_Creature extends Actor {
         else if(rotation == 90) direction = 2;
         else if(rotation == 0) direction = 1;
         else if(rotation == 180) direction = 3;
-        
+
         int w = getImage().getWidth();
         int h = getImage().getHeight();
 
         // 0 for north, 1 for east, 2 for south, 3 for west, 4 for north/south
-        int[] dx = new int[]{-w/2, w/2+1, -w/2, -(w/2+1), w/2};
-        int[] dy = new int[]{-(h/2+1), -h/2, h/2+1, -h/2, h/2};
+        int[] dx = new int[]{-w/2, w/2, -w/2, -(w/2), w/2};
+        int[] dy = new int[]{-(h/2), -h/2, h/2, -h/2, h/2};
 
         setRotation(startRot);
-        
+
         boolean clear = true;
         int counter = 0;
-        
+
         //north or south
         if(direction==0 || direction==2) {
             for(int i = dx[direction]; i<= dx[4]; i++) {
@@ -180,7 +248,50 @@ public class P2_Gupta_Kavya_Creature extends Actor {
         return clear;
 
     }
+
+    public Wall wallAtDirection(int rotation) {
+        //System.out.println(rotation);
+        int startRot = getRotation();
+        setRotation(0);
+        int direction = -1;
+        if(rotation == 270) direction = 0;
+        else if(rotation == 90) direction = 2;
+        else if(rotation == 0) direction = 1;
+        else if(rotation == 180) direction = 3;
+
+        int w = getImage().getWidth();
+        int h = getImage().getHeight();
+
+        // 0 for north, 1 for east, 2 for south, 3 for west, 4 for north/south
+        int[] dx = new int[]{-w/2, w/2+1, -w/2, -(w/2+1), w/2};
+        int[] dy = new int[]{-(h/2+1), -h/2, h/2+1, -h/2, h/2};
+
+        setRotation(startRot);
+
+        boolean clear = true;
+        int counter = 0;
+
+        //north or south
+        if(direction==0 || direction==2) {
+            for(int i = dx[direction]; i<= dx[4]; i++) {
+                i+= counter;
+                if(getOneObjectAtOffset(i, dy[direction], Wall.class) != null) {
+                    return (Wall)getOneObjectAtOffset(i, dy[direction], Wall.class);
+                }
+            }
+        } else {
+            //east or west
+            for(int i = dy[direction]; i<= dy[4]; i++) {
+                i+= counter;
+                clear = getOneObjectAtOffset(dx[direction], i, Wall.class) == null;
+                if(getOneObjectAtOffset(dx[direction], i, Wall.class) != null) {
+                    return (Wall)getOneObjectAtOffset(dx[direction], i, Wall.class);
+                }
+            }
+        }
+
+        return null;
+
+    }
 }
-
-
 
